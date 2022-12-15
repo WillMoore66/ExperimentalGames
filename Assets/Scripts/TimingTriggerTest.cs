01519cc1203bc34efd96c8de5dada21538de0134
Duskaneer <willmoore666666@gmail.com>
2022-12-15 08:22:07 +0000

    moved sound files, added timing system

---
+++ Assets/Scripts/TimingTriggerTest.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimingTriggerTest : MonoBehaviour
+{
+    [SerializeField] int preStumbleTicks;
+    [SerializeField] int successTicks;
+    [SerializeField] int postStumbleTicks;
+
+    uint counter;
+    bool dogInTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "dog")
+        {
+            counter = 0;
+            dogInTrigger = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (dogInTrigger)
+        {
+            counter++;
+
+            //if (player says the right word)
+            //{
+
+            if (counter <= 30 /*&& player says the right word*/)
+            {
+                //dog stumbles before reaching obstacle
+            }
+            else if (counter <= 90 /*&& player says the right word*/)
+            {
+                //dog successfully gets through the obstacle
+            }
+            else if (counter <= 120)
+            {
+                //dog stumbles after reaching obstacle
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "dog")
+        {
+            dogInTrigger = false;
+        }
+    }
+}
