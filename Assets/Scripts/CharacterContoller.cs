8af38f4162ff9af6f49f4b62e1b1702a523e6a0e
WillMoore66 <91475934+WillMoore66@users.noreply.github.com>
2023-01-19 13:09:44 +0000

    Created character controller

---
+++ Assets/Scripts/CharacterContoller.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows.Speech;
+
+public class CharacterContoller : MonoBehaviour
+{
+    [SerializeField] GameObject sphere;
+    private Dictionary<string, Action> keywords = new Dictionary<string, Action>();
+    private KeywordRecognizer keywordRecognizer;
+
+    PlayerInput playerInput;
+    [SerializeField] float theSpEEdOftheDog = 30;
+    InputAction forwardAction;
+    InputAction jumpAction;
+    InputAction turnAction;
+
+    bool jumping;
+    bool turning;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        forwardAction = playerInput.actions["WS"];
+        forwardAction.Enable();
+        jumpAction = playerInput.actions["Jump"];
+        jumpAction.Enable();
+        turnAction = playerInput.actions["Turn"];
+        turnAction.Enable();
+
+        keywords.Add("forward", GoForward);
+        keywords.Add("backwards", GoForward);
+        keywords.Add("jump", Jump);
+        keywords.Add("left", Turn);
+        keywords.Add("right", Turn);
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognised;
+        keywordRecognizer.Start();
+    }
+
+    void Update()
+    {
+        if (forwardAction.ReadValue<float>() != 0)
+        {
+            GoForward();
+        }
+
+        if (jumpAction.ReadValue<float>() != 0)
+        {
+            Jump();
+        }
+
+        if (turnAction.ReadValue<float>() != 0)
+        {
+            Turn();
+        }
+    }
+
+    private void OnKeywordsRecognised(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Keyword: " + args.text);
+        keywords[args.text].Invoke();
+    }
+
+    void GoForward()
+    {
+        this.transform.position += this.transform.forward * theSpEEdOftheDog * forwardAction.ReadValue<float>() * Time.deltaTime;
+    }
+
+    void Turn()
+    {
+        StartCoroutine("TurnRoutine");
+    }
+
+    IEnumerator TurnRoutine()
+    {
+        if (!turning)
+        {
+            turning = true;
+            if (turnAction.ReadValue<float>() == 1)
+            {
+                //turn left
+                sphere.transform.position = (this.transform.position + (this.transform.right * theSpEEdOftheDog / 3));
+                for (int i = 0; i < 45; i++)
+                {
+                    yield return new WaitForFixedUpdate();
+                    this.transform.RotateAround(sphere.transform.position, sphere.transform.up, 2);
+                }
+            }
+            else if (turnAction.ReadValue<float>() == -1)
+            {
+                //turn right
+                sphere.transform.position = (this.transform.position -(this.transform.right * theSpEEdOftheDog / 3));
+                for (int i = 0; i < 45; i++)
+                {
+                    yield return new WaitForFixedUpdate();
+                    this.transform.RotateAround(sphere.transform.position, sphere.transform.up, -2);
+                }
+            }
+            turning = false;
+        }
+    }
+
+    void Jump()
+    {
+        StartCoroutine("JumpUp");
+    }
+
+    IEnumerator JumpUp()
+    {
+        if (!jumping)
+        {
+            jumping = true;
+            sphere.transform.position = (new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z) + this.transform.forward * theSpEEdOftheDog / 3);
+            for (int i = 0; i < 90; i++)
+            {
+                yield return new WaitForFixedUpdate();
+                this.transform.RotateAround(sphere.transform.position, sphere.transform.right, 2);
+                this.transform.Rotate(-this.transform.right * 2);
+            }
+            jumping = false;
+        }
+    }
+}
