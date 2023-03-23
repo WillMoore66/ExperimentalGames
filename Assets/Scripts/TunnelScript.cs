7edad692d956cdfa69ac578de941a7cf7fff8cdb
Callum Holliman <callumholliman02@gmail.com>
2023-03-23 04:56:26 +0000

    added comments to tunnel script, resets tunnel camera when entering tunnel

--- Assets/Scripts/TunnelScript.cs
+++ Assets/Scripts/TunnelScript.cs
@@ -17,15 +17,21 @@ public class TunnelScript : MonoBehaviour
     }
 
     IEnumerator CameraMove() {
+        //set tunnel camera to viewpoint above tunnel entrance, swap from main camera to tunnel camera, disable dog during tunnel
+        tunnelCam.transform.position = new Vector3(333.37f, 10.2f, -49.56f);
         mainCam.enabled = false;
         tunnelCam.enabled = true;
         dog.SetActive(false);
-
+        
+        //spawn sphere between tunnel entrance and exit, then rotate tunnel camera around the midpoint
         sphere.transform.position = (sphere.transform.position = Vector3.Lerp(tunnelEntrance.transform.position, tunnelEnd.transform.position, 0.5f));
+        
         for (int i = 0; i < 180; i++) {
             yield return new WaitForFixedUpdate();
             tunnelCam.transform.RotateAround(sphere.transform.position, sphere.transform.up, 1); 
         }
+
+        //swap back to main camera, set dog's new position to tunnel exit, reenable dog
         tunnelCam.enabled = false;
         mainCam.enabled = true;
         dog.transform.position = tunnelEnd.transform.position;
