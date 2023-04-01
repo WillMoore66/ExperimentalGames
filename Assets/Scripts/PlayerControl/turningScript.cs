d869eed9e1636330b00385ff279305cf63a90ddb
WillMoore66 <91475934+WillMoore66@users.noreply.github.com>
2023-04-01 02:33:13 +0100

    Changed Turning mechanic
    Broken atm as you can only do one turn and only to the left

---
+++ Assets/Scripts/PlayerControl/turningScript.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class turningScript : MonoBehaviour
+{
+    // The amount of torque to apply per frame
+    public float torque = 10f;
+
+    // The direction of torque to apply per frame
+    public Vector3 torqueDirection = Vector3.up;
+
+    public float targetAngle = 90f;
+    private float currentAngle = 0f;
+    private Rigidbody rb;
+    private bool turning;
+    private bool startedTurning;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+
+        if (startedTurning) 
+        {
+            Turn();
+        }
+    }
+
+    void Turn()
+    {
+        // Check if the current angle is less than the target angle
+        if (currentAngle < targetAngle)
+        {
+            // Apply torque to the rigidbody
+            rb.AddTorque(torqueDirection * torque);
+
+            // Update the current angle by calculating the delta angle between the previous and current rotation
+            currentAngle += Quaternion.Angle(transform.rotation, rb.rotation);
+            turning = true; 
+        }
+        else
+        {
+            // Stop applying torque
+            torque = 0f;
+            rb.angularVelocity = Vector3.zero;
+            turning = false;
+        }
+    }
+}
