113a181e6caf0c4d263955bc5a42b653a683ded5
Declan-S <91475504+Declan-S@users.noreply.github.com>
2023-05-04 18:18:59 +0100

    Music and hat changes
    help

--- Assets/Scripts/SoundTesting/PlayBackgroundMusic.cs
+++ Assets/Scripts/SoundTesting/PlayBackgroundMusic.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayBackgroundMusic : MonoBehaviour
 {
+    [SerializeField] private string musicID = "HowToPlayTheme";
+
     // Start is called before the first frame update
     void Start()
     {
-        AudioSource the = SoundManager.current.PlaySound("HowToPlayTheme",SoundManager.current.soundCategories[SoundManager.current.GetCategoryIndexFromID("music")]);
+        AudioSource the = SoundManager.current.PlaySound(musicID,SoundManager.current.soundCategories[SoundManager.current.GetCategoryIndexFromID("music")]);
         the.loop = true;
     }
 }
