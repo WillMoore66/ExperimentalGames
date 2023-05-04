29592ee6d1280870fca08e4d4ee7637096d532c5
Spectrum-Edge <s4107812@glos.ac.uk>
2023-05-04 20:29:44 +0100

    Changed sound registry and fixed menu
    Someone left the hat menu open in the main scene
    Also the dog makes a jump sound but it can't be heard because the listener is broken

--- Assets/Scripts/PlayerControl/NewCharacterController.cs
+++ Assets/Scripts/PlayerControl/NewCharacterController.cs
@@ -225,6 +225,9 @@ public class NewCharacterController : MonoBehaviour
         {
             if (isGrounded && !busy)
             {
+                // Audio
+                SoundManager.current.PlaySound("Jump",this.gameObject,true);
+
                 Vector3 force = new Vector3(0, Mathf.SmoothStep(0, jumpHeight, 1.5f), 0);
                 rb.AddForce(force);
 
